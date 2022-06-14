769ed0fed9903c86cac0921301cc41357c85b948
Benjamin Fuller <benisfull@gmail.com>
2022-06-13 21:50:04 -0700

    Add linked list file

--- LinkedList.java
+++ LinkedList.java
@@ -1,2 +1,10 @@
 public class LinkedList {
+    Node head; // list head
+    //node - linkedlist
+    class Node {
+        int data;
+        Node next;
+        //constructor to create a new node
+        Node(int d) { data = d; }
+    }
 }
