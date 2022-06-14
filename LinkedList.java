ca7557554375a8c639552da258402675226cf842
Benjamin Fuller <benisfull@gmail.com>
2022-06-13 22:28:00 -0700

    implement linked list initial

--- LinkedList.java
+++
@@ -1,4 +0,0 @@
-public class LinkedList {
-    Node head; // list head
-    Node tail;
-}
---
+++ src/LinkedList.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class LinkedList {
+    private Node first;
+    private Node last;
+    private int N;
+
+    public LinkedList() {
+        first = null;
+        last = null;
+        N = 0;
+    }
+
+    public void add(String item) {
+        if (item == null) { System.out.print("The first argument for addLast() is null."); }
+        if (!isEmpty()) {
+            Node prev = last;
+            last = new Node(item, null);
+            prev.next = last;
+        }
+        else {
+            last = new Node(item, null);
+            first = last;
+        }
+        N++;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+}
