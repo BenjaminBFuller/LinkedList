01b0aa44acf847c36a693c860da114ec6a8b0036
TristinTTT <38142359+TristinTTT@users.noreply.github.com>
2022-06-13 22:13:57 -0700

    Node Class Implemented

--- Node.java
+++ Node.java
@@ -1,2 +1,48 @@
-public class Node {
+public class Node
+{
+    private Node prevNode;
+    private Node nextNode;
+    private String value;
+
+    public Node(String valueObject) {
+        prevNode = null;
+        nextNode = null;
+        value = valueObject;
+    }
+
+    public Node( Node prev, String valueObject) {
+        prevNode = prev;
+        nextNode = null;
+        value = valueObject;
+    }
+    public Node( Node prev, Node next, String valueObject){
+        //Assigns the
+        prevNode = prev;
+        nextNode = next;
+        value = valueObject;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public void setPrevNode(Node prevNode) {
+        this.prevNode = prevNode;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public Node getPrevNode() {
+        return prevNode;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
