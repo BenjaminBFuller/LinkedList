50515a92496e0b239687cbc6542b231e6639ed0b
TristinTTT <38142359+TristinTTT@users.noreply.github.com>
2022-06-14 22:42:49 -0700

    Added test case definitions for LinkedListTest

--- src/test/LinkedListTest.java
+++ src/test/LinkedListTest.java
@@ -1,5 +1,102 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
 
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void testAddAll() {
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void element() {
+    }
+
+    @Test
+    void poll() {
+    }
+
+    @Test
+    void pollFirst() {
+    }
+
+    @Test
+    void pollLast() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void testRemove1() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void removeFirstOccurrence() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+    @Test
+    void removeLastOccurrence() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
 }
